2b36d45f3a8de1d0690fc81bb189462b5e0217da
NAKATANI Ichiro <nobitahouse@gmail.com>
2017-01-23 11:44:11 +0900

    Contact の factory_girl を追加して書き換えた
    ファクトリのヘルパーメソッドを追加
    
    ファクトリの検証を追加
    
    factory_girl のメソッドで書き換えた
    
    factory_girl に書き換えた

---
+++ spec/factories/contacts.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :contact do
+    firstname 'John'
+    lastname 'Doe'
+    sequence(:email) { |n| "johndone#{n}@example.com" }
+  end
+end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -2,42 +2,46 @@
 require "rails_helper"
 
 RSpec.describe Contact do
-  it '姓と名とメール有れば有効な状態であること' do
-    contact = Contact.new(
-      firstname: 'Aaron',
-      lastname: 'Sumner',
-      email: 'tester@example.com'
-    )
-    expect(contact).to be_valid
+
+  it '有効なファクトリを持つこと' do
+    expect(build :contact).to be_valid
   end
+
+  it '姓と名とメール有れば有効な状態であること' do
+    contact = build(:contact,
+                    firstname: 'Aaron',
+                    lastname: 'Sumner',
+                    email: 'tester@example.com')
+    expect(build :contact).to be_valid
+  end
+
   it '名がなければ無効な状態であること' do
-    contact = Contact.new(firstname: nil)
+    contact = build(:contact, firstname: nil)
     contact.valid?
     expect(contact.errors[:firstname]).to include("can't be blank")
   end
+
   it '姓がなければ無効な状態であること' do
-    contact = Contact.new(lastname: nil)
+    contact = build(:contact, lastname: nil)
     contact.valid?
     expect(contact.errors[:lastname]).to include("can't be blank")
   end
 
   it 'メールアドレスがなければ無効な状態であること' do
-    contact = Contact.new(email: nil)
+    contact = build(:contact, email: nil)
     contact.valid?
     expect(contact.errors[:email]).to include("can't be blank")
   end
 
   it '重複したメールアドレスなら無効な状態であること' do
-    Contact.create(firstname: 'Joe', lastname: 'Tester',
-                   email: 'tester@example.com')
-    contact = Contact.new(firstname: 'Jane', lastname: 'Tester',
-                          email: 'tester@example.com')
+    create(:contact, email: 'tester@example.com')
+    contact = build(:contact, email: 'tester@example.com')
     contact.valid?
     expect(contact.errors[:email]).to include('has already been taken')
   end
+
   it '連絡先のフルネームを文字列として返すこと' do
-    contact = Contact.new(firstname: 'John', lastname: 'Doe',
-                          email: 'johndone@example.com')
+    contact = build :contact
     expect(contact.name).to eq 'John Doe'
   end
 
