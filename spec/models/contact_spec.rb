c2e6c6a6b6227ecc0d5a44f63e5921b4c5d9e134
NAKATANI Ichiro <nobitahouse@gmail.com>
2017-01-23 09:56:54 +0900

    姓と名、および名のバリデーションを追加

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -2,8 +2,19 @@
 require "rails_helper"
 
 RSpec.describe Contact do
-  it '姓と名とメール有れば有効な状態であること'
-  it '名がなければ無効な状態であること'
+  it '姓と名とメール有れば有効な状態であること' do
+    contact = Contact.new(
+      firstname: 'Aaron',
+      lastname: 'Sumner',
+      email: 'tester@example.com'
+    )
+    expect(contact).to be_valid
+  end
+  it '名がなければ無効な状態であること' do
+    contact = Contact.new(firstname: nil)
+    contact.valid?
+    expect(contact.errors[:firstname]).to include("can't be blank")
+  end
   it '姓がなければ無効な状態であること'
   it 'メールアドレスがなければ無効な状態であること'
   it '重複したメールアドレスなら無効な状態であること'
