e1bad5ec4dcef6d41688dbef9e5ec1bbc964c766
NAKATANI Ichiro <nobitahouse@gmail.com>
2017-01-24 11:01:56 +0900

    ユーザーのファクトリを追加

---
+++ spec/factories/users.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+FactoryGirl.define do
+  factory :user do
+    email { Faker::Internet.email }
+    password 'secret'
+    password_confirmation 'secret'
+  end
+  factory :admin do
+    admin true
+  end
+end
