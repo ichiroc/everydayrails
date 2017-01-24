4dc1f0f6a93a96d46ae664ca7bfbdce1209daf4f
NAKATANI Ichiro <nobitahouse@gmail.com>
2017-01-24 11:19:40 +0900

    表記を変更

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -2,8 +2,8 @@
 # frozen_string_literal: true
 require 'rails_helper'
 
-feature 'ユーザー管理' do
-  scenario '新しいユーザーを追加する' do
+feature 'ユーザー管理' do # MEMO: feature は describe と同等
+  scenario '新しいユーザーを追加する' do # MEMO: it と同等
     admin = create :admin
     visit root_path
     click_link 'Log In'
@@ -12,14 +12,15 @@ feature 'ユーザー管理' do
     click_button 'Log In'
     visit root_path
 
-    expect do
+    expect {
       click_link 'Users'
       click_link 'New User'
       fill_in 'Email', with: 'newuser@example.com'
       find('#password').fill_in 'Password', with: 'secret123'
-      find('#password_confirmation').fill_in 'Password confirmation', with: 'secret123'
+      find('#password_confirmation').fill_in 'Password confirmation',
+                                             with: 'secret123'
       click_button 'Create User'
-    end.to change(User, :count).by(1)
+    }.to change(User, :count).by(1)
 
     expect(current_path).to eq users_path
     expect(page).to have_content 'New user created'
