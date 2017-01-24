b3671ca0f3a7dc7c70a8060f5da2e3bc2c7e2d58
NAKATANI Ichiro <nobitahouse@gmail.com>
2017-01-24 12:10:01 +0900

    JavsScript を使ってシナリオを評価するようにした

--- spec/features/about_us_spec.rb
+++ spec/features/about_us_spec.rb
@@ -2,21 +2,21 @@
 require "rails_helper"
 
 feature 'About BigCo modal' do
-  scenario 'aboutのモーダル表示を切り替える' do
+  scenario 'aboutのモーダル表示を切り替える', js: true do
     visit root_path
-    # JavaScript ドライバを使用していないので下記の example は失敗する
+
     expect(page).not_to have_content 'About BigCo'
-    expect(page).not_to have_content 'BigCo produces the finest widgets in all the land'
+    expect(page).not_to have_content 'BigCo produces the finest widgets in all the land.'
 
     click_link 'About Us'
     expect(page).to have_content 'About BigCo'
-    expect(page).to have_content 'BigCo produces the finest widgets in all the land'
+    expect(page).to have_content 'BigCo produces the finest widgets in all the land.'
 
     within '#about_us' do
       click_button 'Close'
     end
 
     expect(page).not_to have_content 'About BigCo'
-    expect(page).to have_content 'BigCo produces the finest widgets in all the land'
+    expect(page).not_to have_content 'BigCo produces the finest widgets in all the land.'
   end
 end
