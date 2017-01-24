04bf22b1b0b4e1206420861780d9eb546cffeced
NAKATANI Ichiro <nobitahouse@gmail.com>
2017-01-24 11:28:18 +0900

    MEMO コメントを追加

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -12,10 +12,14 @@ feature 'ユーザー管理' do # MEMO: feature は describe と同等
     click_button 'Log In'
     visit root_path
 
+    # MEMO: expect{ hoge }.to change(receiver, message).by(count)
+    # 処理hoge を行うことによって receiver.message の値が count 変わる
+    # by は変更できる
     expect {
       click_link 'Users'
       click_link 'New User'
       fill_in 'Email', with: 'newuser@example.com'
+      # MEMO: CSS, XPath セレクタが使える。 ここでは Password では絞れないので使用している。
       find('#password').fill_in 'Password', with: 'secret123'
       find('#password_confirmation').fill_in 'Password confirmation',
                                              with: 'secret123'
@@ -24,6 +28,7 @@ feature 'ユーザー管理' do # MEMO: feature は describe と同等
 
     expect(current_path).to eq users_path
     expect(page).to have_content 'New user created'
+    # MEMO: 特定のタグの中に操作を限定できる
     within 'h1' do
       expect(page).to have_content 'Users'
     end
